950d2911540ed8e81319731fe362639bb6b885a7
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-11-13 21:50:56 +0100

    CRUD terminado

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodosController extends Controller
+{
+    /**
+     * index para mostrar todos los todos
+     * store para guardar un todo
+     * update para actualizar un todo
+     * destroy para eliminar un todo
+     * edit para mostrar el formulario de edición
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+        ]);
+
+        $todo = new Todo();
+        $todo->title = $request->title;
+        $todo->save();
+        return redirect()->route('todos')->with('success', 'Tarea creada correctamente');
+    }
+
+    public function index()
+    {
+        $todos = Todo::all(); 
+        return view('todos.index', ['todos' => $todos]); 
+    }
+
+    public function show($id){
+        $todo = Todo::find($id);
+        return view('todos.show', ['todo' => $todo]);
+    }
+    
+    public function update(Request $request, $id){
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->save();
+    
+        // return view('todos.index', ['success' => 'Tarea actualizada!']);
+        return redirect()->route('todos')->with('success', 'Tarea actualizada!');
+    }
+    
+    
+    public function destroy($id){
+        $todo = Todo::find($id);
+        $todo->delete();
+    
+        return redirect()->route('todos')->with('success', 'Tarea Eliminada!');
+    }
+    
+    
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todos', function () {
-    return view('todos.index');
-});
+// Ruta para mostrar los todos
+Route::get('/tareas', [TodosController::class, 'index'])->name('todos');
+
+// Ruta para almacenar un nuevo todo
+Route::post('/tareas', [TodosController::class, 'store'])->name('todos');
+
+Route::get('/tareas/{id}', [TodosController::class, 'show'])->name('todos-edit');
+
+Route::patch('/tareas/{id}', [TodosController::class, 'update'])->name('todos-update');
+
+Route::delete('/tareas/{id}', [TodosController::class, 'destroy'])->name('todos-destroy');
