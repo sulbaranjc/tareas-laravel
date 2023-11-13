51c0fb64598595bfd129f176b010c5c7c61417ac
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-11-13 18:10:51 +0100

    Add form to create new task in todo list

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,5 +1,16 @@
 @extends('app');
 
 @section('content')
-    Hola Mundo
+
+<div class="container w-25 border p-4 mt-4">
+    <form>
+        <div class="mb-3">
+            <label for="title" class="form-label">Título de la tarea</label>
+            <input type="text" name="title" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
+    </form>
+</div>
+
+
 @endsection
